741d9e01be26c06849b6f34f042a1012665ed55a
Kouki Ooyatsu <kouki-o@iij.ad.jp>
2013-03-05 15:00:23 +0900

    code refactoring(#initialize, #match)

--- mrbgems/mruby-regexp-pcre/src/mruby_regexp_pcre.c
+++ mrbgems/mruby-regexp-pcre/src/mruby_regexp_pcre.c
@@ -9,68 +9,66 @@
 #include <string.h>
 #include <pcre.h>
 
-typedef struct mrb_regexp_pcre {
+struct mrb_regexp_pcre {
   pcre *re;
-  int flags;
-} mrb_regexp_pcre;
+};
 
 void
 mrb_regexp_free(mrb_state *mrb, void *ptr)
 {
-  mrb_regexp_pcre *mrb_re = ptr;
+  struct mrb_regexp_pcre *mrb_re = ptr;
 
-  pcre_free(mrb_re->re);
+  if (mrb_re->re)
+    pcre_free(mrb_re->re);
   mrb_free(mrb, mrb_re);
 }
 static struct mrb_data_type mrb_regexp_type = { "Regexp", mrb_regexp_free };
 
 static int
-mrb_pcre_convert_flags(mrb_value flags)
+mrb_mruby_to_pcre_options(mrb_value options)
 {
-  int cflags = PCRE_DOTALL;
+  int coptions = PCRE_DOTALL;
 
-  if (mrb_nil_p(flags)) {
-    cflags = 0;
-  } else if (mrb_fixnum_p(flags)) {
-    int nflag = mrb_fixnum(flags);
-    if (nflag & 1) cflags |= PCRE_CASELESS;
-    if (nflag & 2) cflags |= PCRE_EXTENDED;
-    if (nflag & 4) cflags |= PCRE_MULTILINE;
-  } else if (mrb_string_p(flags)) {
-      if (strchr(RSTRING_PTR(flags), 'i')) cflags |= PCRE_CASELESS;
-      if (strchr(RSTRING_PTR(flags), 'x')) cflags |= PCRE_EXTENDED;
-      if (strchr(RSTRING_PTR(flags), 'm')) cflags |= PCRE_MULTILINE;
-  } else if (mrb_type(flags) == MRB_TT_TRUE) {
-    cflags |= PCRE_CASELESS;
+  if (mrb_nil_p(options)) {
+    coptions = 0;
+  } else if (mrb_fixnum_p(options)) {
+    int nopt;
+    nopt = mrb_fixnum(options);
+    if (nopt & 1) coptions |= PCRE_CASELESS;
+    if (nopt & 2) coptions |= PCRE_EXTENDED;
+    if (nopt & 4) coptions |= PCRE_MULTILINE;
+  } else if (mrb_string_p(options)) {
+      if (strchr(RSTRING_PTR(options), 'i')) coptions |= PCRE_CASELESS;
+      if (strchr(RSTRING_PTR(options), 'x')) coptions |= PCRE_EXTENDED;
+      if (strchr(RSTRING_PTR(options), 'm')) coptions |= PCRE_MULTILINE;
+  } else if (mrb_type(options) == MRB_TT_TRUE) {
+    coptions |= PCRE_CASELESS;
   }
 
-  return cflags;
+  return coptions;
 }
 
 mrb_value
 regexp_pcre_initialize(mrb_state *mrb, mrb_value self)
 {
-  int erroff = 0;
+  int erroff = 0, coptions;
   const char *errstr = NULL;
-  mrb_regexp_pcre *reg = NULL;
-  mrb_value source, flag = mrb_nil_value();
+  struct mrb_regexp_pcre *reg = NULL;
+  mrb_value source, opt = mrb_nil_value();
 
-  mrb_get_args(mrb, "S|o", &source, &flag);
+  mrb_get_args(mrb, "S|o", &source, &opt);
 
-  reg = mrb_malloc(mrb, sizeof(mrb_regexp_pcre));
-  memset(reg, 0, sizeof(mrb_regexp_pcre));
+  reg = mrb_malloc(mrb, sizeof(struct mrb_regexp_pcre));
+  memset(reg, 0, sizeof(struct mrb_regexp_pcre));
   DATA_PTR(self) = reg;
   DATA_TYPE(self) = &mrb_regexp_type;
+  coptions = mrb_mruby_to_pcre_options(opt);
 
-  reg->flags = mrb_pcre_convert_flags(flag);
-
-  reg->re = pcre_compile(RSTRING_PTR(source), reg->flags, &errstr, &erroff, NULL);
+  reg->re = pcre_compile(mrb_str_to_cstr(mrb, source), coptions, &errstr, &erroff, NULL);
   if (reg->re == NULL) {
     mrb_raisef(mrb, E_ARGUMENT_ERROR, "invalid regular expression");
   }
-
   mrb_iv_set(mrb, self, mrb_intern(mrb, "@source"), source);
-  mrb_iv_set(mrb, self, mrb_intern(mrb, "@options"), mrb_fixnum_value(reg->flags));
 
   return self;
 }
@@ -79,35 +77,48 @@ regexp_pcre_initialize(mrb_state *mrb, mrb_value self)
 mrb_value
 regexp_pcre_match(mrb_state *mrb, mrb_value self)
 {
-  int i, nmatch = OVECCOUNT, match[OVECCOUNT], rc;
+  int i, rc;
+  int nmatch;
+  int *match;
   struct RClass *m;
   mrb_value md, str;
   mrb_int pos;
-  mrb_regexp_pcre *reg;
+  struct mrb_regexp_pcre *reg;
   mrb_value args[2];
 
-  reg = (mrb_regexp_pcre *)mrb_get_datatype(mrb, self, &mrb_regexp_type);
+  reg = (struct mrb_regexp_pcre *)mrb_get_datatype(mrb, self, &mrb_regexp_type);
   if (!reg)
     return mrb_nil_value();
 
   mrb_get_args(mrb, "S|i", &str, &pos);
 
-  m = mrb_class_get(mrb, "MatchData");
-  md = mrb_class_new_instance(mrb, 0, NULL, m);
-
-  rc = pcre_exec(reg->re, NULL, RSTRING_PTR(str), strlen(RSTRING_PTR(str)), 0, 0, match, nmatch);
+  rc = pcre_fullinfo(reg->re, NULL, PCRE_INFO_CAPTURECOUNT, &nmatch);
   if (rc < 0) {
+    /* fullinfo error */
     return mrb_nil_value();
   }
 
-  for (i = 0; i < rc; i++) {
+  nmatch = (nmatch + 1) * 3; /* capture(nmatch) + substring(1) * multiple 3 */
+  match = mrb_malloc(mrb, sizeof(int) * nmatch);
+  rc = pcre_exec(reg->re, NULL, mrb_str_to_cstr(mrb, str), RSTRING_LEN(str), 0, 0, match, nmatch);
+  if (rc < 0) {
+    free(match);
+    return mrb_nil_value();
+  }
+
+  m = mrb_class_get(mrb, "MatchData");
+  md = mrb_class_new_instance(mrb, 0, NULL, m);
+  for (i = 0; i < nmatch; i++) {
     args[0] = mrb_fixnum_value(match[2 * i]);
     args[1] = mrb_fixnum_value(match[2 * i + 1]);
     mrb_funcall_argv(mrb, md, mrb_intern(mrb, "push"), sizeof(args) / sizeof(args[0]), &args[0]);
   }
+
+  mrb_iv_set(mrb, md, mrb_intern(mrb, "@regexp"), self);
   mrb_iv_set(mrb, md, mrb_intern(mrb, "@length"), mrb_fixnum_value(rc));
   mrb_iv_set(mrb, md, mrb_intern(mrb, "@string"), mrb_str_dup(mrb, str));
 
+  free(match);
   return md;
 }
 
