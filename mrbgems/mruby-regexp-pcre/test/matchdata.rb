c59b23e3ccfc6c9fa614717ec82c943b402e5fa3
Tomoyuki Sahara <tsahara@iij.ad.jp>
2013-03-06 11:47:00 +0900

    tests for methods in ISO Ruby.

--- mrbgems/mruby-regexp-pcre/test/matchdata.rb
+++ mrbgems/mruby-regexp-pcre/test/matchdata.rb
@@ -34,3 +34,40 @@ assert('MatchData#length', '15.2.16.3.6') do
   m = /(foo)(bar)(BAZ)?/.match("foobarbaz")
   m.length == 4
 end
+
+assert('MatchData#offset', '15.2.16.3.7') do
+  m = /(foo)(bar)(BAZ)?/.match("foobarbaz")
+  p m.offset(2)
+  p m.begin(2), m.end(2), m.offset(1)
+  m.offset(1) == [0, 3] and m.offset(2) == [3, 6] and m.offset(3) == [nil, nil]
+end
+
+assert('MatchData#post_match', '15.2.16.3.8') do
+  m = /(bar)(BAZ)?/.match("foobarbaz")
+  m.post_match == "baz"
+end
+
+assert('MatchData#pre_match', '15.2.16.3.9') do
+  m = /(bar)(BAZ)?/.match("foobarbaz")
+  m.pre_match == "foo"
+end
+
+assert('MatchData#size', '15.2.16.3.10') do
+  m = /(foo)(bar)(BAZ)?/.match("foobarbaz")
+  m.size == m.length
+end
+
+assert('MatchData#string', '15.2.16.3.11') do
+  m = /b/.match("abc", 1)
+  m.string == "abc"
+end
+
+assert('MatchData#to_a', '15.2.16.3.12') do
+  m = /(foo)(bar)(BAZ)?/.match("foobarbaz")
+  m.to_a == ["foobar", "foo", "bar", nil]
+end
+
+assert('MatchData#to_a', '15.2.16.3.13') do
+  m = /bar/.match("foobarbaz")
+  m.to_s == "bar"
+end
