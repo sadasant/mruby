b7828efb72ecb10e7e85a7b2d098aeff536dfe20
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-03-01 18:13:10 +0900

    allow send method not to call mrb_funcall if calling method is implemented in Ruby; fix #1680 ref #1765

--- src/backtrace.c
+++ src/backtrace.c
@@ -73,6 +73,7 @@ mrb_output_backtrace(mrb_state *mrb, struct RObject *exc, output_stream_func fun
     filename = NULL;
     lineno = -1;
 
+    if (!ci->proc) continue;
     if (MRB_PROC_CFUNC_P(ci->proc)) {
       continue;
     }
