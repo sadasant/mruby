918f5365eda7c7c3409a75af685a743d1f96a6f5
cremno <cremno@mail.ru>
2014-03-05 19:50:13 +0100

    error.h: add extern "C" for C++
    Conflicts:
    
    	include/mruby/error.h

--- src/error.h
+++ src/error.h
@@ -7,6 +7,10 @@
 #ifndef MRUBY_ERROR_H
 #define MRUBY_ERROR_H
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 void mrb_sys_fail(mrb_state *mrb, const char *mesg);
 int sysexit_status(mrb_state *mrb, mrb_value err);
 mrb_value mrb_exc_new_str(mrb_state *mrb, struct RClass* c, mrb_value str);
@@ -15,5 +19,9 @@ mrb_value mrb_make_exception(mrb_state *mrb, int argc, mrb_value *argv);
 mrb_value mrb_format(mrb_state *mrb, const char *format, ...);
 void mrb_exc_print(mrb_state *mrb, struct RObject *exc);
 void mrb_longjmp(mrb_state *mrb);
+
+#if defined(__cplusplus)
+}  /* extern "C" { */
+#endif
 
 #endif  /* MRUBY_ERROR_H */
